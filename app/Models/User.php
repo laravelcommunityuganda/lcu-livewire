c57605b23f5a3460d4683d12a1ca8b3402f20b5c
laravelcommunityuganda <cybehad@gmail.com>
2025-06-09 01:48:41 +0300

    fix: added initials() function in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -97,5 +98,16 @@ class User extends Authenticatable
     public function isOrganizer(): bool
     {
         return $this->role === 'organizer' || $this->isAdmin();
+    }
+     /**
+     * Get the user's initials
+     */
+    public function initials(): string
+    {
+        return Str::of($this->name)
+            ->explode(' ')
+            ->take(2)
+            ->map(fn ($word) => Str::substr($word, 0, 1))
+            ->implode('');
     }
 }
